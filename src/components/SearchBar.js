ad1257e5aa8bb00ad7eee220a32cdd3ecf07163b
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-19 14:17:36 -0300

    captura o evento do input na SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,20 @@ import { Segment, Input } from 'semantic-ui-react'
 
 class SearchBar extends Component {
 
+    pesquisaTermo = e => {
+        // captura o valor digitado no input
+        if (e.keyCode === 13) {
+            const termo = e.target.value
+            console.log(e.target.value)
+        }
+    }
+
     render() {
         return (
 
             <div className="search-bar">
                 <Segment stacked>
-                    <Input icon='search' size='large' placeholder='Search...' />
+                    <Input icon='search' onKeyDown={(e) => this.pesquisaTermo(e)} size='large' placeholder='Search...' />
                 </Segment>
             </div>
 
