ed63764b5b3979654e5e0ba66d0d37c6dfa09a5e
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-21 10:33:18 -0300

    conecta componente VideoList ao state da store

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { List, Image } from 'semantic-ui-react'
 
 const VideoList = props => {
     return (
 
         <div className="video-list">
-            <h1>Video List</h1>
+            <List animated verticalAlign="middle">
+                <List.Item>
+                    <Image avatar src='' />
+                    <List.Content>
+                        <List.Header>Título do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+
+            <p>{JSON.stringify(props)}</p>
         </div>
 
     )
 }
 
-export default VideoList;
+// traz o state da store(global) para o componente
+const mapStateToProps = (state) => {
+    return {
+        videos: state.busca.videos,
+        carregando: state.busca.carregando,
+        erro: state.busca.erro
+    }
+}
+
+// connect usado para conectar o component ao redux
+// com isso o componente tem acesso ao estado da aplicação
+export default connect(mapStateToProps, null)(VideoList);
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger'
 
 import reducers from './reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const middlewares = [thunk, logger]
 
 const store = createStore(reducers, composeEnhancers(
